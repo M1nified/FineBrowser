e0e997f53f97ae2d07dfa95044832708cde3b61b
M!nified <gora.pub@gmail.com>
2016-02-06 16:00:15 +0100

    differences to file

--- samples/files_supervisor.php
+++ samples/files_supervisor.php
@@ -7,6 +7,11 @@ echo 'FilesSupervisor example';
 $filesSupervisor = new FilesSupervisor(".");
 $filesSupervisor->sum_to_file('.','md5s.csv');
 // $filesSupervisor->sum_to_file();
+
 $changes = $filesSupervisor->cmp_sum_files('files_supervisor_md5_2016-02-06-15-04-18.csv','md5s.csv');
+ob_start();
 print_r($changes);
+$output = ob_get_contents();
+ob_end_flush();
+file_put_contents('differences.txt',$output);
 ?>
